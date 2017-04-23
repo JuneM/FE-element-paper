aea739e51323f612f5155554ebd0d11ee5dafb62
June <JuneMzs@163.com>
2017-04-23 23:55:58 +0800

    添加less

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,7 @@ var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -64,7 +65,21 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
-      }
+      },
+      {  
+        test: /\.css$/,                  
+        // 将样式抽取出来为独立的文件
+        loader: ExtractTextPlugin.extract({fallback:"style-loader", use:["css-loader","postcss-loader"]}),
+        exclude: /node_modules/
+      },
+      // 使用less-loader、autoprefixer-loader、css-loader和style-loade 加载 .less 结尾的文件
+      {  
+        test: /\.less$/,                  
+        // 将样式抽取出来为独立的文件scss
+        // use:['style-loader','css-loader','autoprefixer-loader','sass-loader'], 
+        use:['style-loader','css-loader','autoprefixer-loader','less-loader'], 
+        exclude: /node_modules/
+      }        
     ]
   }
 }
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -16,10 +16,5 @@
 </script>
 
 <style scoped>
-  .paper-header {
-    width: 100%;
-    height: 68px;
-    background: rgba(0,0,0,0.50);
-    box-shadow: 0 0 4px 0 rgba(0,0,0,0.08);
-  }
+  @import './Header.less';
 </style>
